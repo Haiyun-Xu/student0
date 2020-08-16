75b2aac84065171a9b926c78b3c8458a46e2c8da
Haiyun-Xu <xuhaiyunhenry@gmail.com>
2020-08-16 19:57:10 +0000

    Completed part 2;

--- hw3/shell.c
+++ hw3/shell.c
@@ -1,3 +1,10 @@
+/**
+ * This is a simple shell program.
+ * 
+ * @author Haiyun Xu <xuhaiyunhenry@gmail.com>
+ * @copyright MIT
+ */
+
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -20,7 +27,8 @@
 bool shell_is_interactive;
 
 /* File descriptor for the shell input */
-int shell_terminal;
+int shell_input;
+int shell_output;
 
 /* Terminal mode settings for the shell */
 struct termios shell_tmodes;
@@ -30,6 +38,8 @@ pid_t shell_pgid;
 
 int cmd_exit(struct tokens *tokens);
 int cmd_help(struct tokens *tokens);
+int cmd_pwd(struct tokens *tokens);
+int cmd_cd(struct tokens *tokens);
 
 /* Built-in command functions take token array (see parse.h) and return int */
 typedef int cmd_fun_t(struct tokens *tokens);
@@ -44,21 +54,83 @@ typedef struct fun_desc {
 fun_desc_t cmd_table[] = {
   {cmd_help, "?", "show this help menu"},
   {cmd_exit, "exit", "exit the command shell"},
+  {cmd_pwd, "pwd", "print the current working directory path"},
+  {cmd_cd, "cd", "change the current working directory"},
 };
 
-/* Prints a helpful description for the given command */
+/**
+ * A built-in command of the shell. Brings up the help menu and prints a helpful description for the given command
+ * 
+ * @return int Returns 0 if command completed successfully, or an error code
+ */
 int cmd_help(unused struct tokens *tokens) {
   for (unsigned int i = 0; i < sizeof(cmd_table) / sizeof(fun_desc_t); i++)
     printf("%s - %s\n", cmd_table[i].cmd, cmd_table[i].doc);
   return 1;
 }
 
-/* Exits this shell */
+/**
+ * A built-in command of the shell. Exits this shell process.
+ * 
+ * @return None Should exit the shell process directly
+ */
 int cmd_exit(unused struct tokens *tokens) {
   exit(0);
 }
 
-/* Looks up the built-in command, if it exists. */
+/**
+ * A built-in command of the shell. Prints the current working directory in the file system.
+ * 
+ * @return int Returns 0 if command completed successfully, or -1 if failed
+ */
+int cmd_pwd(unused struct tokens *tokens) {
+  /*
+   * setting the buffer to NULL and the buffer size to 0 will cause getcwd()
+   * to dynamically allocate a buffer as big as neccessary
+   */
+  char *cwdPathName = getcwd(NULL, 0);
+  if (cwdPathName == NULL) {
+    perror("Failed to get current working directory: ");
+    return -1;
+  }
+  
+  // print the current working directory to standard output
+  size_t bytesWritten = 0, pathNameLength = strlen(cwdPathName);
+  char newline = '\n';
+  while (bytesWritten < pathNameLength)
+    bytesWritten += write(shell_output, (void *)(cwdPathName + bytesWritten), (pathNameLength - bytesWritten));
+  write(shell_output, (void *)&newline, 1);
+
+  free(cwdPathName);
+  return 0;
+}
+
+/**
+ * A built-in command of the shell. Changes the current working directory in the file system.
+ * 
+ * @param tokens The list of command arguments
+ * 
+ * @return int Returns 0 if command completed successfully, or -1 if failed
+ */
+int cmd_cd(struct tokens *tokens) {
+  // the desired working directory should be the second token
+  char *pathName = tokens_get_token(tokens, 1);
+
+  if (chdir(pathName)) {
+    perror("Failed to change current working directory: ");
+    return -1;
+  }
+  
+  return 0;
+}
+
+/**
+ * Look up and return the built-in command index if it exists. Otherwise return -1.
+ * 
+ * @param cmd Pointer to a command string
+ * 
+ * @return int Index of the command in the command table
+ */
 int lookup(char cmd[]) {
   for (unsigned int i = 0; i < sizeof(cmd_table) / sizeof(fun_desc_t); i++)
     if (cmd && (strcmp(cmd_table[i].cmd, cmd) == 0))
@@ -66,29 +138,38 @@ int lookup(char cmd[]) {
   return -1;
 }
 
-/* Intialization procedures for this shell */
+/**
+ * Intialization procedures for this shell.
+ */
 void init_shell() {
   /* Our shell is connected to standard input. */
-  shell_terminal = STDIN_FILENO;
+  shell_input = STDIN_FILENO;
+  shell_output = STDOUT_FILENO;
 
   /* Check if we are running interactively */
-  shell_is_interactive = isatty(shell_terminal);
+  shell_is_interactive = isatty(shell_input);
 
   if (shell_is_interactive) {
-    /* If the shell is not currently in the foreground, we must pause the shell until it becomes a
+    /**
+     * If the shell is not currently in the foreground, we must pause the shell until it becomes a
      * foreground process. We use SIGTTIN to pause the shell. When the shell gets moved to the
-     * foreground, we'll receive a SIGCONT. */
-    while (tcgetpgrp(shell_terminal) != (shell_pgid = getpgrp()))
+     * foreground, we'll receive a SIGCONT.
+     * 
+     * We detect whether the shell is in the foreground by checking if the process owning stadin is
+     * the shell process (compare the process group ids). If not, we send to all processes within the
+     * group a SIGTTIN signal (stop terminal input for background process).
+     */
+    while (tcgetpgrp(shell_input) != (shell_pgid = getpgrp()))
       kill(-shell_pgid, SIGTTIN);
 
     /* Saves the shell's process id */
     shell_pgid = getpid();
 
     /* Take control of the terminal */
-    tcsetpgrp(shell_terminal, shell_pgid);
+    tcsetpgrp(shell_input, shell_pgid);
 
     /* Save the current termios to a variable, so it can be restored later. */
-    tcgetattr(shell_terminal, &shell_tmodes);
+    tcgetattr(shell_input, &shell_tmodes);
   }
 }
 
